1db2703a47f646480ad7a7a8f65dfc4418c0beb2
Polo! <cristianpoloo@gmail.com>
2014-07-09 18:57:39 +0200

    Añadido traductor

--- app/src/main/java/upv/welcomeincoming/app/Fragment_Traduccion.java
+++ app/src/main/java/upv/welcomeincoming/app/Fragment_Traduccion.java
@@ -1,10 +1,19 @@
 package upv.welcomeincoming.app;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import util.Traductor.ApiKeys;
+import util.Traductor.detect.Detect;
+import util.Traductor.language.Language;
+import util.Traductor.translate.Translate;
 
 
 /**
@@ -12,27 +21,61 @@ import android.view.ViewGroup;
  */
 public class Fragment_Traduccion extends Fragment {
 
-
+    TextView txTrad;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_kitdev, container, false);
-        /*ImageButton btn_speech = (ImageButton) view.findViewById(R.id.btn_speech);
-        ImageButton btn_ra = (ImageButton) view.findViewById(R.id.btnRA);
-        btn_speech.setOnClickListener(new View.OnClickListener() {
+        View view = inflater.inflate(R.layout.fragment_traduccion, container, false);
+        Translate.setKey(ApiKeys.YANDEX_API_KEY);
+        Button btn_speech = (Button) view.findViewById(R.id.btn_speech);
+        final EditText editText = (EditText) view.findViewById(R.id.etTrad);
+        txTrad = (TextView) view.findViewById(R.id.tvTrad);
+        editText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Activity_Traduccion.class);
-                startActivity(intent);
+                if (!editText.getText().equals("")) {
+                    new TraductorTask(editText.getText().toString(), null, Language.ENGLISH, true).execute();
+                }
             }
         });
-        btn_ra.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Activity_RealidadAumentada.class);
-                startActivity(intent);
-            }
-        });*/
         return view;
     }
 
+    private class TraductorTask extends AsyncTask<String, Void, Void> {
+        private String textoAtraducir;
+        private Language idiomaOrigen;
+        private Language idiomaDestino;
+        private boolean detectarIdioma;
+        private String textoTraducido;
+
+        private TraductorTask(String texto, Language idiomaOrigen, Language idiomaDestino, boolean detectarIdioma) {
+            textoAtraducir = texto;
+            this.idiomaDestino = idiomaDestino;
+            this.idiomaOrigen = idiomaOrigen;
+            this.detectarIdioma = detectarIdioma;
+        }
+
+        private Exception exception;
+
+        @Override
+        protected void onPostExecute(Void v) {
+
+            txTrad.setText(textoTraducido);
+
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            try {
+                if (detectarIdioma) {
+                    Language idiomaDetectado = Detect.execute(textoAtraducir);
+                    textoTraducido = Translate.execute(textoAtraducir, idiomaDetectado, idiomaDestino);
+                } else {
+                    textoTraducido = Translate.execute(textoAtraducir, idiomaOrigen, idiomaDestino);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
---
+++ app/src/main/java/util/Traductor/ApiKeys.java
@@ -0,0 +1,5 @@
+package util.Traductor;
+
+public class ApiKeys {
+    public static final String YANDEX_API_KEY = "trnsl.1.1.20140708T183503Z.331cae19b01cbe45.4e58d76b5a0dbcc75622ed57f3a6a7f21c6d7a6f";
+}
